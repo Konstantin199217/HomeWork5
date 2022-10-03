574d47789a59632ac1b9b97c4dddb0c909919e39
Konstantin <kot199217@mail.ru>
2022-10-03 12:50:39 +0900

    End Task34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int[] NewArray(int size)
+{
+    int[] Array = new int[size];
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = new Random().Next(100, 999);
+    }
+    return Array;
+}
+
+
+int[] Array = NewArray(ReadData("Введите длинну массива: "));
+
+Console.WriteLine(String.Join(",", Array));
+Console.WriteLine();
+int number = 0;
+for (int i = 0; i <= Array.Length -1; i++)
+{
+    if (Array[i] % 2 == 0) number++;
+}
+Console.WriteLine(number);
+
+
+
+
