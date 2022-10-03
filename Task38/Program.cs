f78ca188f1d4c3370271ede482b6c666447886e4
Konstantin <kot199217@mail.ru>
2022-10-03 14:09:30 +0900

    chenge Task38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,3 +1,14 @@
-﻿
+﻿//Задача 38: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
 
+//[3 7 22 2 78] -> 76
 
+int[] NewArray(int size)
+{
+    int[] Array = new int[size];
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = new Random().Next(0, 20);
+    }
+    return Array;
+}
