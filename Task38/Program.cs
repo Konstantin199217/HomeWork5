44587483c577f983a9dab077b58596eeb859f07e
Konstantin <kot199217@mail.ru>
2022-10-03 15:22:34 +0900

    End task38

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -3,12 +3,61 @@
 
 //[3 7 22 2 78] -> 76
 
-int[] NewArray(int size)
+// Console.WriteLine("Введите длинну массива: ");
+// string n = Console.ReadLine();
+// int n1 = Convert.ToInt32(n);
+
+// double[] Array = new double[n1];
+// double max = 0;
+// double min = 0;
+// for (int i = 0; i < Array.Length; i++)
+// {
+//     Array[i] = new Random().NextDouble() * n1;
+// }
+// if (Array[i] > max)
+//     {
+//         max = Array[i];
+//     }
+//     if (Array[i] < min)
+//     {
+//         min = Array[i];
+//     }
+Console.WriteLine("Введите длинну массива: ");
+
+string n = Console.ReadLine();
+int n1 = Convert.ToInt32(n);
+double [] NewArray(int size)
 {
-    int[] Array = new int[size];
+    double[] Array = new double[size];
     for (int i = 0; i < Array.Length; i++)
     {
-        Array[i] = new Random().Next(0, 20);
+        Array[i] = new Random().NextDouble();
     }
     return Array;
 }
+double[] Array = NewArray(n1);
+double max =Array[0];
+double min =Array[0];
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i] > max)
+     {
+         max = Array[i];
+     }
+}
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i] < min)
+     {
+         min = Array[i];
+     }
+
+}
+
+Console.WriteLine(String.Join(",  ", Array));
+Console.WriteLine(max);
+Console.WriteLine(min);
+Console.WriteLine();
+Console.WriteLine(max - min);
+
+
